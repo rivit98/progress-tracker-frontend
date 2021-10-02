b9760e1385f3056cb76ec32d0c724a002a8a4167
Rivit <albert98g@gmail.com>
2021-10-02 21:26:57 +0200

    update crackmes list

--- pt_frontend/src/components/crackmes/crackmesList.js
+++ pt_frontend/src/components/crackmes/crackmesList.js
@@ -10,11 +10,13 @@ import {
     Box,
     Button,
     Flex,
+    Icon,
     Link,
     List,
     ListIcon,
     ListItem,
-    Select
+    Select,
+    Text
 } from '@chakra-ui/react';
 import formatDate from '../../utils/dateformatter';
 import { useDispatch, useSelector } from 'react-redux';
@@ -22,7 +24,23 @@ import { crackmes, setCrackmes, setTasksLastUpdated } from '../../context/crackm
 import { isLoggedIn } from '../../context/userReducer';
 import { ExternalLinkIcon } from '@chakra-ui/icons';
 import { Link as ReactRouterLink } from 'react-router-dom';
-import { MdArrowDropDown, MdCheckCircle, MdSettings } from 'react-icons/all';
+import {
+    BiHide,
+    FaCheckCircle,
+    FaPlay,
+    FaTimesCircle,
+    FiCheckCircle,
+    FiChevronsUp,
+    FiMinus,
+    FiPlus,
+    MdArrowDropDown,
+    MdCheckCircle,
+    MdSettings,
+    MdUpdate,
+    MdVisibilityOff
+} from 'react-icons/all';
+
+import { Tooltip } from '@chakra-ui/react';
 
 const CrackmeActionsNotLogged = () => {
     return (
@@ -35,7 +53,7 @@ const CrackmeActionsNotLogged = () => {
     );
 };
 
-const statusMap = {
+const statusDesc = {
     0: 'Clear',
     1: 'Started',
     2: 'Aborted',
@@ -43,49 +61,67 @@ const statusMap = {
     4: 'Ignored'
 };
 
+const statusIcon = {
+    0: { icon: FaCheckCircle, color: 'green.700' },
+    1: { icon: FaPlay, color: 'yellow.500' },
+    2: { icon: FaTimesCircle, color: 'red.600' },
+    3: { icon: FaCheckCircle, color: 'green.500' },
+    4: { icon: MdVisibilityOff, color: 'blue.600' }
+};
+
 const ActionsList = ({ crackme }) => {
     const { actions, comments_num, hexid, writeups_num } = crackme;
     const link = `https://crackmes.one/crackme/${hexid}`;
 
     return (
-        <Box>
+        <Box textAlign={'left'}>
             <Flex
-                textAlign="left"
+                textAlign="center"
                 flexDirection={'row'}
-                justifyContent={'space-between'}
+                justifyContent={'center'}
                 w={'full'}
                 mx={'auto'}
                 mb={2}
                 experimental_spaceX={'2'}
             >
-                <Box flex={8} overflow={'hidden'}>
+                <Box flex={1} overflow={'hidden'} flexDirection={'row'} alignItems={'center'} experimental_spaceX={1}>
                     <Link href={link} isExternal color={'teal.500'}>
-                        Challenge link <ExternalLinkIcon textDecoration={'none'} my={'auto'} ml={1} />
+                        Challenge link
                     </Link>
+                    <ExternalLinkIcon />
                 </Box>
-                <Box flex={3} overflow={'hidden'}>
+                <Box flex={1} overflow={'hidden'}>
                     Writeups: {writeups_num}
                 </Box>
-                <Box flex={4} overflow={'hidden'}>
+                <Box flex={1} overflow={'hidden'}>
                     Comments: {comments_num}
                 </Box>
             </Flex>
             <List spacing={3} my={4}>
                 {actions.map((a) => {
+                    const status = a.status;
+                    const { icon, color } = statusIcon[status];
                     return (
                         <ListItem>
-                            <ListIcon as={MdCheckCircle} color="green.500" />
-                            {formatDate(a.date, 'dd.mm.yyyy HH:MM:ss')} status: {statusMap[a.status]}
+                            <ListIcon as={icon} color={color} fontSize={'lg'} />
+                            {formatDate(a.date, 'dd.mm.yyyy HH:MM:ss')} -- {statusDesc[status]}
                         </ListItem>
                     );
                 })}
             </List>
             <Flex flexDirection={'row'} experimental_spaceX={2}>
-                <Select icon={<MdArrowDropDown />} w={'25%'} size={'sm'} bg="teal" color="gray">
+                <Select
+                    icon={<MdArrowDropDown />}
+                    w={'25%'}
+                    size={'sm'}
+                    border={0}
+                    bg="gray.700"
+                    color="whiteAlpha.500"
+                >
                     <option value={'started'}>Start solving</option>
-                    <option value={'started'}>Abort solving</option>
-                    <option value={'started'}>Mark as solved</option>
-                    <option value={'started'}>Mark as ignored</option>
+                    <option value={'aborted'}>Stop solving</option>
+                    <option value={'solved'}>Mark as solved</option>
+                    <option value={'ignored'}>Mark as ignored</option>
                 </Select>
                 <Button colorScheme="teal" fontSize={'md'} fontWeight={'normal'} size={'sm'}>
                     Update status
@@ -99,7 +135,7 @@ const Crackme = ({ crackme }) => {
     const logged = useSelector(isLoggedIn);
 
     //TODO: icon/row color depending on current state??
-    const { date, name, language } = crackme;
+    const { date, name, language, lastAction } = crackme;
 
     return (
         <AccordionItem border={0} maxW={'3xl'} w={'full'} mx={'auto'} textAlign={'center'} isDisabled={false}>
@@ -113,6 +149,13 @@ const Crackme = ({ crackme }) => {
             >
                 <Flex flexDirection={'row'} justifyContent={'space-between'} experimental_spaceX={'2'} w={'full'}>
                     <Box ml={1} flex={8} overflow={'hidden'} textAlign="left">
+                        {lastAction && (
+                            <Icon
+                                as={statusIcon[lastAction.status].icon}
+                                color={statusIcon[lastAction.status].color}
+                                mr={1}
+                            />
+                        )}
                         {name}
                     </Box>
                     <Box flex={3} overflow={'hidden'}>
@@ -121,7 +164,7 @@ const Crackme = ({ crackme }) => {
                     <Box flex={4} overflow={'hidden'}>
                         {language}
                     </Box>
-                    <Box flex={1} textAlign="right" justifyContent={'end'} ml={'auto'} mr={2}>
+                    <Box w={'20px'} textAlign="right" justifyContent={'end'} mr={2}>
                         <AccordionIcon />
                     </Box>
                 </Flex>
@@ -133,12 +176,7 @@ const Crackme = ({ crackme }) => {
     );
 };
 
-export const CrackmesListRenderer = ({ tasks }) => {
-    const {
-        lastUpdated: { created, date, deleted, total_scrapped, updated }
-    } = useSelector(crackmes);
-    const cacheDate = new Date(date);
-
+const CrackmesListRenderer = ({ tasks }) => {
     return (
         <>
             <Flex
@@ -162,7 +200,7 @@ export const CrackmesListRenderer = ({ tasks }) => {
                 <Box flex={4} overflow={'hidden'}>
                     Language
                 </Box>
-                <Box flex={1} overflow={'hidden'} />
+                <Box w={'20px'} />
             </Flex>
             <Flex w={'full'} justifyContent={'center'}>
                 <Accordion allowToggle w={'full'}>
@@ -174,15 +212,38 @@ export const CrackmesListRenderer = ({ tasks }) => {
                         ))}
                 </Accordion>
             </Flex>
-            <Flex>pagination</Flex>
-            <Flex mt={5}>
-                Last updated: {formatDate(cacheDate)} | {created} new | {deleted} removed | {updated} updated |{' '}
-                {total_scrapped} in total
-            </Flex>
+            <Flex mt={2}>pagination</Flex>
+            <UpdateSummary />
         </>
     );
 };
 
+const SummaryLabel = ({ icon, content }) => {
+    return (
+        <Flex mx={1} flexDirection={'row'} alignItems={'center'} experimental_spaceX={3}>
+            {icon}
+            {content}
+        </Flex>
+    );
+};
+
+const UpdateSummary = () => {
+    const {
+        lastUpdated: { created, date, deleted, updated }
+    } = useSelector(crackmes);
+    const cacheDate = new Date(date);
+
+    return (
+        <Flex mt={8}>
+            <Text mr={1}>Last updated: {formatDate(cacheDate)}</Text>
+            <SummaryLabel icon={<FiPlus color={'green'} />} content={created} />
+            <SummaryLabel icon={<FiChevronsUp color={'yellow'} />} content={updated} />
+            <SummaryLabel icon={<FiMinus color={'red'} />} content={deleted} />
+        </Flex>
+    );
+};
+
+//TODO: max cache age
 export const CrackmesList = () => {
     const logged = useSelector(isLoggedIn);
     const dispatch = useDispatch();
@@ -197,7 +258,6 @@ export const CrackmesList = () => {
             return new Promise((resolve) => resolve([]));
         }
 
-        console.log('actions');
         return crackmesService.getActions(options);
     };
 
@@ -233,8 +293,10 @@ export const CrackmesList = () => {
     const actions = stateData[1];
 
     const tasksWithActions = stateData[0].map((t) => {
-        const a = actions[t.id] || [];
-        return { ...t, actions: a.sort((a1, a2) => a2.date.getTime() - a1.date.getTime()) };
+        let a = actions[t.id] || [];
+        a = a.sort((a1, a2) => a2.date.getTime() - a1.date.getTime());
+        const lastAction = a[0] || undefined;
+        return { ...t, actions: a, lastAction: lastAction };
     });
 
     return (
