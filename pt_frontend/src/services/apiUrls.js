45b808fe46dc7d632761085b5fea75a8f04bb367
Rivit <albert98g@gmail.com>
2021-12-10 23:14:22 +0100

    CPT-28 CPT-47 update frontend urls

--- pt_frontend/src/services/apiUrls.js
+++ pt_frontend/src/services/apiUrls.js
@@ -1,14 +1,13 @@
-const API_VERSION = 'v1';
+const REGISTER = `/user/v1/register/`;
+const ME = `/user/v1/me/`;
 
-const REGISTER = `/crackmes/${API_VERSION}/auth/register/`;
-const ME = `/crackmes/${API_VERSION}/users/me/`;
-const TOKEN = `/crackmes/${API_VERSION}/auth/login/`;
-const REFRESH_TOKEN = `/crackmes/${API_VERSION}/auth/refresh/`;
+const TOKEN = `/auth/v1/login/`;
+const REFRESH_TOKEN = `/auth/v1/refresh/`;
 
-const CRACKMES = `/crackmes/${API_VERSION}/tasks/`;
-const LAST_UPDATED = `/crackmes/${API_VERSION}/tasks/lastUpdated/`;
-const ACTIONS = `/crackmes/${API_VERSION}/tasks/actions/`;
-const UPDATE_STATUS = `/crackmes/${API_VERSION}/tasks/%d/status/`;
+const CRACKMES = `/crackmes/v1/`;
+const LAST_UPDATED = `/crackmes/v1/lastUpdated/`;
+const ACTIONS = `/crackmes/v1/actions/`;
+const UPDATE_STATUS = `/crackmes/v1/%d/status/`;
 
 export const apiUrls = {
     REGISTER,
