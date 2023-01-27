96b8da3521aa7da5a49a89f8e78a408f33fdb6d2
Rivit <albert98g@gmail.com>
2023-01-27 21:06:52 +0100

    CPT-110 home page rework

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,51 +1,38 @@
-import { Box, Container, Link, ListItem, Popover, PopoverBody, PopoverContent, PopoverTrigger, UnorderedList, VStack } from '@chakra-ui/react';
+import { Box, Card, CardBody, CardHeader, Center, Container, Divider, Heading, Link, SimpleGrid, VStack } from '@chakra-ui/react';
 import { Link as ReactRouterLink } from 'react-router-dom';
+
+const HomeCard = ({ header, target, children }) => {
+    return <Card as={ReactRouterLink} to={target} bg={'whiteAlpha.800'}>
+        <CardHeader>
+            <Center>
+                <Heading size='md'>{header}</Heading>
+            </Center>
+        </CardHeader>
+        <Divider></Divider>
+        <CardBody>
+            {children}
+        </CardBody>
+    </Card>
+}
 
 const Home = () => {
     return (
         <Container maxW="container.sm">
             <VStack experimental_spaceY={10} alignItems={'start'}>
-                <Box textAlign={'justify'}>
-                Use the panel below on the left side to log in or navigate to the trackers.
-                </Box>
-                <Box>
-                    Currently supported trackers are:
-                    <UnorderedList>
-                        <ListItem>
-                            <Popover trigger={'hover'} openDelay={100} closeOnBlur>
-                                <PopoverTrigger>
-                                    <Link as={ReactRouterLink} to={'/crackmes'} color={'teal.500'}>
-                                        crackmes
-                                    </Link>
-                                </PopoverTrigger>
-                                <PopoverContent bg='gray.700' borderColor='gray.700'>
-                                    <PopoverBody>
-                                        Track progress on{' '}
-                                        <Link href={'https://crackmes.one'} isExternal color={'teal.500'}>
-                                            crackmes.one
-                                        </Link>{' '}
-                                        website. Original page does not allow you to mark challenges as solved or hide the bad ones
-                                        so I made custom tracker for it :)
-                                    </PopoverBody>
-                                </PopoverContent>
-                            </Popover>
-                        </ListItem>
-                        <ListItem>
-                            <Popover trigger={'hover'} openDelay={100} closeOnBlur>
-                                <PopoverTrigger>
-                                    <Link as={ReactRouterLink} to={'/heroes-maps'} color={'teal.500'}>
-                                        heroes maps
-                                    </Link>
-                                </PopoverTrigger>
-                                <PopoverContent bg='gray.700' borderColor='gray.700'>
-                                    <PopoverBody>
-                                        There are so many HoMM maps I want to play so I made a tracker for them so I don't miss any!
-                                    </PopoverBody>
-                                </PopoverContent>
-                            </Popover>
-                        </ListItem>
-                    </UnorderedList>
-                </Box>
+                <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
+                    <HomeCard header={'crackmes'} target={'/crackmes'}>
+                        Tracker for{' '}
+                        <Link href={'https://crackmes.one'} isExternal color={'teal.500'}>
+                            crackmes.one
+                        </Link>.{' '}
+                        Original site does not allow you to mark challenges as solved or hide the bad ones
+                        so I made a custom tracker for it :)
+                    </HomeCard>
+
+                    <HomeCard header={'heroes maps'} target={'/heroes-maps'}>
+                        There are so many HoMM maps I want to play so fancy tracker for them would be useful!
+                    </HomeCard>
+                </SimpleGrid>
             </VStack>
         </Container>
     );
