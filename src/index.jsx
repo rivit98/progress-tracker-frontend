2088708f9bb3bf349aee748fb92adf8708d534cc
Rivit <albert98g@gmail.com>
2023-02-01 23:36:18 +0100

    remove unused css

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { setupAxios } from './utils/axiosConfig';
 
+setupAxios();
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
-setupAxios();
